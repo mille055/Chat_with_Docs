c71e83578c3ace00925aa6cdf5f35631060a7a62
mille055 <46400739+mille055@users.noreply.github.com>
2024-02-04 19:43:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,15 +12,18 @@ def get_text(pdf_files):
 
     Returns:
         text (str): The text extracted from the PDF file
+        text_dict (dict): The text extracted for each page number
     """
-  
+    text_dict = {}
     text = ""
     for pdf_file in pdf_files:
         print("Extracting text from {}".format(pdf_file.name))
         pdf_reader = PdfReader(pdf_file)
 
-        # Extract the text from the PDF pages and add it to the raw text variable
-        for page in pdf_reader.pages:
-            text += page.extract_text()
+        # Extract the text from the PDF pages and add it to a dictionary
+        for index, page in enumerate(pdf_reader.pages):
+            current_page_text = page.extract_text()
+            text_dict[index]=current_page_text
+            text += current_page_text
     
-    return text
+    return text, text_dict
